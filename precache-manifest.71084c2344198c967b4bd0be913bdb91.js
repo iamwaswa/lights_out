83fc447ade0b1fe2bf8ddbf1b82661b7bb072762
waswaolunga <olungaw@gmail.com>
2019-07-20 13:22:39 -0700

    Updates

---
+++ precache-manifest.71084c2344198c967b4bd0be913bdb91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f8678a28ec394cd6ac8",
+    "url": "/lights_out/static/js/runtime~main.9f8678a2.js"
+  },
+  {
+    "revision": "b76d9deac4a0f4f50c4b",
+    "url": "/lights_out/static/js/main.b76d9dea.chunk.js"
+  },
+  {
+    "revision": "8c17a0c8425ce492d4c6",
+    "url": "/lights_out/static/js/1.8c17a0c8.chunk.js"
+  },
+  {
+    "revision": "b76d9deac4a0f4f50c4b",
+    "url": "/lights_out/static/css/main.02386541.chunk.css"
+  },
+  {
+    "revision": "ceff50f2e4ef9c71b17f0e704e5587e2",
+    "url": "/lights_out/index.html"
+  }
+];
